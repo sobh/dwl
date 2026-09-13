5fd19feaff54209bcb3a12017040a80ada4e32b1
Guido Cella <guido@guidocella.xyz>
2026-09-13 21:14:10 +0200

    support monitors above and below in focusmon and tagmon
    Fixes https://codeberg.org/dwl/dwl/issues/1105

--- dwl.c
+++ dwl.c
@@ -1474,7 +1474,8 @@ dirtomon(enum wlr_direction dir)
 			dir, selmon->wlr_output, selmon->m.x, selmon->m.y)))
 		return next->data;
 	if ((next = wlr_output_layout_farthest_output(output_layout,
-			dir ^ (WLR_DIRECTION_LEFT|WLR_DIRECTION_RIGHT),
+			dir ^ (WLR_DIRECTION_LEFT | WLR_DIRECTION_RIGHT |
+					WLR_DIRECTION_UP | WLR_DIRECTION_DOWN),
 			selmon->wlr_output, selmon->m.x, selmon->m.y)))
 		return next->data;
 	return selmon;
